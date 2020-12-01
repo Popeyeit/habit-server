5ff98f552c1c5d4bbc0c4bcccb9abeea08ccd10f
PopeyeIT <igorserov28@gmail.com>
2020-12-01 21:52:27 +0200

    change condition on correct token

--- src/user/controllers.js
+++ src/user/controllers.js
@@ -29,7 +29,13 @@ exports.authorize = async (req, res, next) => {
     }
 
     const user = await userModel.findById(userId);
-    if (!user && token !== user.token) {
+
+    if (!user) {
+      res.status(401).json('Not authorized');
+      return;
+    }
+
+    if (token !== user.token) {
       res.status(401).json('Not authorized');
       return;
     }
